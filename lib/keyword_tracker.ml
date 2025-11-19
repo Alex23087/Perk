c623cc98c637cb88903d4e22a31bcfa4a0d6e633
axdelafuen <axel.dlf63@gmail.com>
2025-11-19 21:22:33 +0000

    remove magic number to use keywords list size

--- lib/keyword_tracker.ml
+++ lib/keyword_tracker.ml
@@ -6,7 +6,7 @@ let last_keyword_clear () = last_keyword := None
 let last_keyword_set (s : string) = last_keyword := Some s
 
 let reserved_set = 
-  let tbl = Hashtbl.create 97 in
+  let tbl = Hashtbl.create (List.length reserved_keywords) in
   List.iter (fun kw -> Hashtbl.add tbl kw ()) reserved_keywords;
   tbl
 
@@ -14,8 +14,7 @@ let is_reserved (s : string) : bool = Hashtbl.mem reserved_set s
 
 let check_identifier (ctx : string) (id : string) : unit =
   if is_reserved id then
-    raise (Errors.ParseError(!Utils.fnm, 
-      Printf.sprintf "keyword '%s' cannot be used as %s identifier" id ctx))
+    raise (Errors.ParseError(!Utils.fnm, Printf.sprintf "keyword '%s' cannot be used as %s identifier" id ctx))
 
 let validate_var_identifier = check_identifier "variable"
 let validate_fun_identifier = check_identifier "function"
@@ -26,6 +25,5 @@ let validate_struct_identifier = check_identifier "struct"
 
 let raise_keyword_error (fnm : string ref) (ctx : string) =
   match !last_keyword with
-  | Some kw when is_reserved kw ->
-      raise (Errors.ParseError(!fnm, Printf.sprintf "keyword '%s' cannot be used as %s identifier" kw ctx))
+  | Some kw when is_reserved kw -> raise (Errors.ParseError(!fnm, Printf.sprintf "keyword '%s' cannot be used as %s identifier" kw ctx))
   | _ -> raise (Errors.ParseError(!fnm, ctx ^ " expected"))
